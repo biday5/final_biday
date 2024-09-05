2fbdc78a981757c9d7a850f3ec355c3a60e84a8f
pokitong <yujusul@gmail.com>
2024-09-05 20:29:35 +0900

    feat : 프론트 프로젝트 파일 추가

---
+++ biday-front/src/app/(page)/shull/wish/list/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import * as api from "@/app/service/wish/wish.api";
+
+function wishList() {
+
+    return (
+        <></>
+    );
+}
+
+export default wishList();
---
+++ biday-front/src/app/model/AuctionModel.ts
@@ -0,0 +1,3 @@
+interface AuctionModel {
+    id?: number;
+}
