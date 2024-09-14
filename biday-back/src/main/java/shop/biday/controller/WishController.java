a38012943a165643fc9a46049050930eb52a7bc9
pokitong <yujusul@gmail.com>
2024-09-14 16:12:13 +0900

    feat: 위시 기기능 추가

--- biday-back/src/main/java/shop/biday/controller/WishController.java
+++ biday-back/src/main/java/shop/biday/controller/WishController.java
@@ -2,10 +2,13 @@ package shop.biday.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import shop.biday.model.repository.WishRepository;
 import shop.biday.service.WishService;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -14,4 +17,45 @@ import shop.biday.service.WishService;
 @Tag(name = "wishes", description = "Wish Controller")
 public class WishController {
     private final WishService wishService;
+    private final WishRepository wishRepository;
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<?>> findByUser(@PathVariable Long id) {
+
+        List<?> wishList = wishRepository.findByUserId(id);
+        System.out.println("wishList = " + wishList);
+
+        if (wishList == null || wishList.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(wishList);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> toggleWish(@RequestParam Long userId, @RequestParam Long productId) {
+
+        boolean result = wishService.toggleWIsh(userId, productId);
+
+        if (result) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("위시 생성 성공");
+        } else {
+            return ResponseEntity.ok("위시 삭제 성공");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+
+        System.out.println("deleteMapping");
+
+        try {
+            wishRepository.deleteById(id);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 wishId: " + id);
+        }
+
+        return ResponseEntity.ok("위시 삭제 성공");
+    }
 }
--- biday-back/src/main/java/shop/biday/service/impl/WishServiceImpl.java
+++ biday-back/src/main/java/shop/biday/service/impl/WishServiceImpl.java
@@ -3,10 +3,59 @@ package shop.biday.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import shop.biday.model.entity.ProductEntity;
+import shop.biday.model.entity.UserEntity;
+import shop.biday.model.entity.WishEntity;
+import shop.biday.model.repository.WishRepository;
 import shop.biday.service.WishService;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class WishServiceImpl implements WishService {
+
+    private final WishRepository wishRepository;
+
+    @Override
+    public boolean toggleWIsh(Long userId, Long productId) {
+
+        boolean isWish = isWish(userId, productId);
+        if (isWish) {
+            deleteWish(userId, productId);
+
+            return false;
+        } else {
+            insertWish(userId, productId);
+
+            return true;
+        }
+    }
+
+    @Override
+    public void deleteWish(Long userId, Long productId) {
+        wishRepository.deleteWish(userId, productId);
+    }
+
+    @Override
+    public void insertWish(Long userId, Long productId) {
+        UserEntity user = UserEntity.builder().id(userId).build();
+        ProductEntity product = ProductEntity.builder().id(productId).build();
+
+        WishEntity wish = WishEntity.builder()
+                .user(user)
+                .product(product)
+                .build();
+
+        wishRepository.save(wish);
+    }
+
+    @Override
+    public boolean isWish(Long userId, Long productId) {
+        WishEntity result = wishRepository.findByUserIdAndProductId(userId, productId);
+
+        if (result == null) {
+            return false;
+        } else return true;
+
+    }
 }
